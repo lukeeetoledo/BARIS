58cf32c49b91c54817392255c6b3e9e1a8d3dc70
jawo406 <95694835+jawo406@users.noreply.github.com>
2022-04-09 09:05:59 +0800

    up

--- index.php
+++ index.php
@@ -13,10 +13,8 @@
   <link rel="stylesheet" href="CSS/index.css">
   <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-  <link rel="stylesheet" href="aaa/index.css">
   <link rel="manifest" href="manifest.json">
   <link rel="apple-touch-icon" href="img/Logo_192.png">
-
 </head>
 
 <body>
