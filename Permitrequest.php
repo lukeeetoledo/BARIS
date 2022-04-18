8bfb9ac8557894dcb6d67f11a2f98a0287125a0b
jawo406 <95694835+jawo406@users.noreply.github.com>
2022-04-18 08:50:51 +0800

    up

--- Permitrequest.php
+++ Permitrequest.php
@@ -15,8 +15,7 @@
     <div class="d-flex" id="wrapper">
         <!-- Sidebar -->
         <div class="bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i
-                    class="fas fa-user-secret me-2"></i>BARIS</div>
+            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><img src="img/FINAL.png" alt="" width="60" height="60">BARIS</div>
             <div class="list-group list-group-flush my-3">
                 <a href="barangayside.php" class="list-group-item list-group-item-action bg-transparent second-text active"><i
                         class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
